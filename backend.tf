d43e2e63770f00f62bea2cb494a408143c12567d
Pepu <pepshove.com>
2025-03-04 18:14:53 +0200

    fixed 6

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-bucket"
+    bucket = "my-terraform-state-bucket1214"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
